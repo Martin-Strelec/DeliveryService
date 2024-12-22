12104cf2c631587f9ad429a695491e0d1b52dd13
Martin-Strelec <m.strelec2021@gmail.com>
2024-12-22 15:22:41 +0000

    Added orderSuccess.js

---
+++ js/orderSuccess.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    updateValues();
+})
+function updateValues() {
+    //Local storage return values
+    const getCart = localStorage.getItem('cart');
+
+    //Selecting elements on the page
+    const currentUserIcon = document.getElementById('currentUser');
+    const cartCount = document.getElementById('cartCount');
+
+    var tempTotal = 0;
+    var tempCount = 0;
+
+    if (getCart !== '[]') {
+        const getCart = JSON.parse(localStorage.getItem('cart'));
+
+        getCart.forEach(item => {
+            const itemCount = document.getElementById(`${item.Type}-count`);
+            if (itemCount !== null) {
+                itemCount.textContent = `${item.count}`;
+            }
+
+            tempTotal += item.count * item.Price;
+            tempCount += item.count;
+        })
+    }
+    else {
+        tempTotal = 0;
+        tempCount = 0;
+    }
+
+    cartCount.innerHTML = tempCount;
+
+    displayUser(currentUserIcon);
+}
+function displayUser(element) {
+    if (localStorage.getItem('currentUser') !== '') {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        element.setAttribute('class', 'bg-primary rounded-3 p-1 ms-1 text-white small');
+        element.textContent = `${currentUser.uName}`;
+    }
+}
